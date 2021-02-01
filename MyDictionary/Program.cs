48436e6c29c3328f48d0d593a6aeb6a61d4410ed
enesbora <boraenes84@gmail.com>
2021-02-01 13:25:39 +0100

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> dictionary = new MyDictionary<string, string>();
+
+            dictionary.Add("car", "araba");
+            dictionary.Add("knife", "bicak");
+            dictionary.Add("cat", "kedi");
+            dictionary.Add("muz", "banana");
+            dictionary.Items();
+        }
+    }
+}
